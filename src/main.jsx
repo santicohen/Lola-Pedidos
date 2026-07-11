d1a363e423f9d713f407e12f875de7713cc1d0fb
santicohen <30536738+santicohen@users.noreply.github.com>
2026-07-11 19:46:45 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./OrderEntry.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
