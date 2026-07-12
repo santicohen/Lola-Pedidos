0c0a36f3d48e263c6d847a37627e1810a1031017
santicohen <30536738+santicohen@users.noreply.github.com>
2026-07-11 21:24:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // IMPORTANTE: 'base' debe coincidir con el nombre del repositorio de GitHub.
 // Si tu repo se llama distinto a "lola-pedidos", cambiá el valor de abajo.
 export default defineConfig({
-  base: "/lola-pedidos/",
+  base: "/Lola-Pedidos/",
   plugins: [react()],
 });
